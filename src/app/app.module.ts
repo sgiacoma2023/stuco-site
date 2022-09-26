6cdf0d6e0477131628af0712aad2367163a73b12
Luc Mainvielle <lmainvielle2023@chadwickschool.org>
2022-09-26 11:05:33 -0700

    chore: Add firebase code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   imports: [
+   
+
     BrowserModule,
     AppRoutingModule
   ],
