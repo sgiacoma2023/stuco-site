4e9b492363a5836b481d0ca51f7b915cb52f59ff
Luc Mainvielle <lmainvielle2023@chadwickschool.org>
2022-09-26 11:30:59 -0700

    chore: firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,24 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDS25AgYBoYZIJE_e4YChkwVIkFP_tPd5A",
+
+  authDomain: "stuco-site.firebaseapp.com",
+
+  projectId: "stuco-site",
+
+  storageBucket: "stuco-site.appspot.com",
+
+  messagingSenderId: "288191456658",
+
+  appId: "1:288191456658:web:56fa131a7cfd9afc730d4b",
+
+  measurementId: "G-P59G2LKF92"
+
+};
+
 
 @NgModule({
   declarations: [
